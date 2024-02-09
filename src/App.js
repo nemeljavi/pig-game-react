690dc0635f3a8380c0ecc784962bc34b1b0a370c
nemeljavi <nemeljavi70@gmail.com>
2024-02-09 14:34:16 +0100

    definir variables de estado con useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import "./App.css";
+import { useState } from "react";
 import Player from "./Player/Player";
+
+// 1. definir variables de estado usando useState (activePlayer, score, current, diceNumber)
+// 2. definir funciones para manejar los eventos de click (handleNewGame, handleRollDice, handleHold)
+// 3. pasar las variables de estado y las funciones a los componentes Player y Dice
+// 4. manejar los eventos de click en los botones de New game, Roll dice y Hold
+// 5. manejar el cambio de imagen de dado cuando se hace click en el botón rolldice
+// 5. manejar el cambio de jugador activo cuando se hace click en el botón Hold
+// 6. manejar el cambio de jugador activo cuando se obtiene un 1 al hacer click en el botón Roll dice
+// 7. manejar el cambio de jugador activo cuando se obtiene un 6 al hacer click en el botón Roll dice
+// 8. manejar el cambio de jugador activo cuando se obtiene un número diferente de 1 o 6 al hacer click en el botón Roll dice
+// 9. manejar el cambio de jugador activo cuando se hace click en el botón New game
+
 function App() {
+  const [activePlayer, setActivePlayer] = useState(2);
+  const [score, setScore] = useState([3, 6]);
+  const [current, setCurrent] = useState(7);
+  const [diceNumber, setDiceNumber] = useState(3);
+
   return (
     <main>
-      <Player name="Player1" score={103} current={0} isActive={true} />
-      <Player name="Player2" score={24} current={0} isActive={false} />
-      <img src="dice-5.png" alt="Playing dice" className="dice" />
+      <Player
+        name="Player 1"
+        score={score[0]}
+        current={activePlayer === 1 && current}
+        isActive={activePlayer === 1}
+      />
+      <Player
+        name="Player 2"
+        score={score[1]}
+        current={activePlayer === 2 && current}
+        isActive={activePlayer === 2}
+      />
+      {diceNumber && (
+        <img
+          src={`dice-${diceNumber}.png`}
+          alt="Playing dice"
+          className="dice"
+        />
+      )}
       <button className="btn btn--new">
-        :flechas_en_sentido_antihorario: New game
+        :arrows_counterclockwise: New game
       </button>
-      <button className="btn btn--roll">:dado: Roll dice</button>
-      <button className="btn btn--hold">:bandeja_de_entrada: Hold</button>
+      <button className="btn btn--roll">:game_die: Roll dice</button>
+      <button className="btn btn--hold">:inbox_tray: Hold</button>
     </main>
   );
 }
+
 export default App;
